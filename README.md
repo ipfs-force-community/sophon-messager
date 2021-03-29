7243a5ba3bd293b7e6057c6ad5f066bf50a0ed75
Mike <41407352+hunjixin@users.noreply.github.com>
2021-03-29 18:17:26 +0800

    update readme (#38)

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # venus-messager
-message is component of venus for managing message 
+
+messager is a component used to manage local messages, with the purpose of saving address messages, managing message status, and controlling the frequency of push messages.
+
+## Work
+
+✅Support for sqlite and mysql storage
+✅Connect multiple wallets and scan wallet addresses.
+✅Connect node components to maintain the status of messages that have been sent.
+✅maintain the status of address(nonce)
+✅Simple message selection push and nonce assignment
+❌global Gas evaluation parameters, address push quantity configuration
+❌Message multi-point push (pushed directly to the blockchain network with libp2p, push to the node by Mpool API)
+❌API Security
+❌Rich and flexible message sorting options
+❓Manage messages through a multi-tenant pattern by wallet name
+
+
+## Getting Start
+
+build binary
+```sh
+git clone 
+make deps
+make
+```
+
+edit messager.toml config file, edit node url and token
+
+```sh
+./venus-messager -config ./messager.toml
+```
+
+## client
+
--- cmds/client_example/main.go
+++ cmds/client_example/main.go
@@ -4,11 +4,6 @@ import (
 	"context"
 	"fmt"
 
-	"log"
-	"math/rand"
-	"net/http"
-	"time"
-
 	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
@@ -16,10 +11,11 @@ import (
 	"github.com/ipfs-force-community/venus-messager/api/client"
 	"github.com/ipfs-force-community/venus-messager/config"
 	"github.com/ipfs-force-community/venus-messager/types"
+	"log"
+	"net/http"
 )
 
 func main() {
-	//cfg, err := config.ReadConfig("/Users/lijunlong/Desktop/workload/venus-messager/messager.toml")
 	cfg, err := config.ReadConfig("./messager.toml")
 	if err != nil {
 		log.Fatal(err)
@@ -34,50 +30,35 @@ func main() {
 	}
 
 	defer closer()
-	addr1, _ := address.NewFromString("t3qtatmg6tsxolkrbpbb63lexcxgcph4pujowihkayxx23fonnztfspjhviejflu6ssjitqmx3sei5k63ul5la")
-	addr2, _ := address.NewFromString("t3qtatmg6tsxolkrbpbb63lexcxgcph4pujowihkayxx23fonnztfspjhviejflu6ssjitqmx3sei5k63ul5la")
+	from, _ := address.NewFromString("t3v3wx6tbwlvzev7hxhbpdlfwvwq5mbdhfrgmy2i2ztfaqhwjwc6zkxo6to4x2ms2acicd3x57fabxhpszzwqq")
+	to, _ := address.NewFromString("t3ru4e5hrvhsjjvyxyzzxzmsoahrdmobsfz6ohmd7ftswxyf7dxvhnmkq63cu5ozdy4wnrcqxx4gkwa427grga")
 
-	addrs := []address.Address{addr1, addr2}
-	tm := time.NewTicker(time.Second * 1)
-	/*for i:=0 ;i< 100;i++ {
-		fmt.Println(rand.Intn(2))
-	}*/
-	//return
-	for range tm.C {
-		for i := 0; i < 50; i++ {
-			from := addrs[rand.Intn(2)]
-			to := addrs[rand.Intn(2)]
-			fmt.Println(from)
-			msgMate := &types.MsgMeta{
-				ExpireEpoch:       abi.ChainEpoch(1000000 + i),
-				GasOverEstimation: 1.25,
-				MaxFee:            big.NewInt(10000000000000000),
-				MaxFeeCap:         big.NewInt(10000000000000000),
-			}
-			if i < 5 {
-				msgMate.ExpireEpoch = 0
-			}
-			uid, err := client.PushMessageWithId(context.Background(),
-				types.NewUUID().String(),
-				&venustypes.UnsignedMessage{
-					Version: 0,
-					To:      from,
-					From:    to,
-					Nonce:   1,
-					Value:   abi.NewTokenAmount(100),
-					Method:  0,
-				},
-				msgMate)
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-
-			fmt.Println("send message " + uid)
-		}
+	fmt.Println(from)
+	msgMate := &types.MsgMeta{
+		ExpireEpoch:       abi.ChainEpoch(1000000),
+		GasOverEstimation: 1.25,
+		MaxFee:            big.NewInt(10000000000000000),
+		MaxFeeCap:         big.NewInt(10000000000000000),
+	}
+	uid, err := client.PushMessageWithId(context.Background(),
+		types.NewUUID().String(),
+		&venustypes.UnsignedMessage{
+			Version: 0,
+			To:      from,
+			From:    to,
+			Nonce:   1,
+			Value:   abi.NewTokenAmount(100),
+			Method:  0,
+		},
+		msgMate)
+	if err != nil {
+		log.Fatal(err)
+		return
 	}
 
-	/*msg, err := client.WaitMessage(context.Background(), uid, 5)
+	fmt.Println("send message " + uid)
+
+	msg, err := client.WaitMessage(context.Background(), uid, 5)
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -88,5 +69,5 @@ func main() {
 	fmt.Println("gas_used:", msg.Receipt.GasUsed)
 	fmt.Println("return_value:", msg.Receipt.ReturnValue)
 	fmt.Println("Height:", msg.Height)
-	fmt.Println("Tipset:", msg.TipSetKey.String())*/
+	fmt.Println("Tipset:", msg.TipSetKey.String())
 }
