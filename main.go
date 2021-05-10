34ca718091cb066a399ff5f57497ac1e4fc8c733
hunjixin <1084400399@qq.com>
2021-05-10 16:33:13 +0800

    fix unable to read config after upgrade dependency

--- main.go
+++ main.go
@@ -27,6 +27,14 @@ func main() {
 	app := &cli.App{
 		Name:  "venus message",
 		Usage: "used for manage message",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "config",
+				Aliases: []string{"c"},
+				Value:   "./messager.toml",
+				Usage:   "specify config file",
+			},
+		},
 		Commands: []*cli.Command{ccli.MsgCmds,
 			ccli.AddrCmds,
 			ccli.WalletCmds,
@@ -49,13 +57,6 @@ var runCmd = &cli.Command{
 	Name:  "run",
 	Usage: "run messager",
 	Flags: []cli.Flag{
-		&cli.StringFlag{
-			Name:    "config",
-			Aliases: []string{"c"},
-			Value:   "./messager.toml",
-			Usage:   "specify config file",
-		},
-
 		&cli.StringFlag{
 			Name:  "auth-url",
 			Usage: "url for auth server",
