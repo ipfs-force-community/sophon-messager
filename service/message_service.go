6202d2de9e1a454ae2885673e875d0c7d0382be4
Mike <41407352+hunjixin@users.noreply.github.com>
2021-05-10 19:05:54 +0800

    fix unable to read config after upgrade dependency (#92)
    * fix unable to read config after upgrade dependency
    
    * fix log
    
    * rm unused code
    
    * add go mod download in ci

--- service/message_service.go
+++ service/message_service.go
@@ -612,7 +612,7 @@ func (ms *MessageService) multiNodeToPush(ctx context.Context, msgs []*venusType
 			}
 		}
 		ms.log.Infof("start to broadcast message of address")
-		for fromAddr, _ := range fromMap {
+		for fromAddr := range fromMap {
 			if err := node.cli.MpoolPublishByAddr(ctx, fromAddr); err != nil {
 				ms.log.Errorf("publish message of address %s to node %s fail %v", fromAddr, node.name, err)
 			}
@@ -771,6 +771,9 @@ func (ms *MessageService) ReplaceMessage(ctx context.Context, id string, auto bo
 	}
 	err = ms.messageState.MutatorMessage(msg.ID, func(message *types.Message) error {
 		message.SignedCid = msg.SignedCid
+		message.GasLimit = msg.GasLimit
+		message.GasPremium = msg.GasPremium
+		message.GasFeeCap = msg.GasFeeCap
 		message.UnsignedCid = msg.UnsignedCid
 		message.UnsignedMessage = msg.UnsignedMessage
 		message.State = msg.State
