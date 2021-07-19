7f6b0f127eed8b21b5ee8f4da9f7562a7fe1b904
simlecode <hj1557986@gmail.com>
2021-07-19 16:50:12 +0800

    remove unused field

--- types/shared_params.go
+++ types/shared_params.go
@@ -1,23 +1,17 @@
 package types
 
 import (
-	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/big"
 )
 
 type SharedParams struct {
 	ID uint `json:"id"`
 
-	ExpireEpoch       abi.ChainEpoch `json:"expireEpoch"`
-	GasOverEstimation float64        `json:"gasOverEstimation"`
-	MaxFee            big.Int        `json:"maxFee,omitempty"`
-	MaxFeeCap         big.Int        `json:"maxFeeCap"`
+	GasOverEstimation float64 `json:"gasOverEstimation"`
+	MaxFee            big.Int `json:"maxFee,omitempty"`
+	MaxFeeCap         big.Int `json:"maxFeeCap"`
 
 	SelMsgNum uint64 `json:"selMsgNum"`
-
-	ScanInterval int `json:"scanInterval"` // second
-
-	MaxEstFailNumOfMsg uint64 `json:"maxEstFailNumOfMsg"`
 }
 
 func (sp *SharedParams) GetMsgMeta() *MsgMeta {
@@ -25,7 +19,6 @@ func (sp *SharedParams) GetMsgMeta() *MsgMeta {
 		return nil
 	}
 	return &MsgMeta{
-		ExpireEpoch:       sp.ExpireEpoch,
 		GasOverEstimation: sp.GasOverEstimation,
 		MaxFee:            sp.MaxFee,
 		MaxFeeCap:         sp.MaxFeeCap,
