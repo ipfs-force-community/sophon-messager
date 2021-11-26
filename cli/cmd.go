68184ae5cd5a99e5829050dae81f33164dff6d73
zl <zl03jsj@163.com>
2021-11-26 16:10:32 +0800

    search message command display params and returns

--- cli/cmd.go
+++ cli/cmd.go
@@ -1,7 +1,12 @@
 package cli
 
 import (
+	"context"
+	"github.com/filecoin-project/go-address"
 	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/venus-messager/service"
+	"github.com/filecoin-project/venus-messager/utils/actor_parser"
+	"github.com/filecoin-project/venus/pkg/types"
 	"github.com/ipfs-force-community/venus-common-utils/apiinfo"
 	"github.com/urfave/cli/v2"
 
@@ -24,4 +29,32 @@ func getAPI(ctx *cli.Context) (client.IMessager, jsonrpc.ClientCloser, error) {
 	client, closer, err := client.NewMessageRPC(ctx.Context, addr, apiInfo.AuthHeader())
 
 	return client, closer, err
+}
+
+func getNodeAPI(ctx *cli.Context) (*service.NodeClient, jsonrpc.ClientCloser, error) {
+	cfg, err := config.ReadConfig(ctx.String("config"))
+	if err != nil {
+		return &service.NodeClient{}, func() {}, err
+	}
+	return service.NewNodeClient(ctx.Context, &cfg.Node)
+}
+
+func getActorGetter(ctx *cli.Context) (actor_parser.ActorGetter, jsonrpc.ClientCloser, error) {
+	nodeAPI, closer, err := getNodeAPI(ctx)
+	if err != nil {
+		return nil, nil, err
+	}
+	return &getter{NodeClient: nodeAPI}, closer, nil
+}
+
+type getter struct {
+	*service.NodeClient
+}
+
+func (g *getter) StateGetActor(ctx context.Context, addr address.Address, tsKey types.TipSetKey) (*types.Actor, error) {
+	return g.NodeClient.StateGetActor(ctx, addr, tsKey)
+}
+
+func (g *getter) StateLookupID(ctx context.Context, addr address.Address, tsKey types.TipSetKey) (address.Address, error) {
+	return g.NodeClient.StateLookupID(ctx, addr, tsKey)
 }
