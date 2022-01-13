1c899f1224087f2e4375348d3179c3c072d67c50
hunjixin <1084400399@qq.com>
2022-01-13 17:52:53 +0800

    fix msg detail marshal

--- types/msg.go
+++ types/msg.go
@@ -1,6 +1,8 @@
 package types
 
 import (
+	"encoding/json"
+	"github.com/filecoin-project/go-address"
 	"time"
 
 	"github.com/filecoin-project/go-state-types/abi"
@@ -54,6 +56,71 @@ type Message struct {
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+//todo ignore use message MarshalJSON method
+func (m *Message) MarshalJSON() ([]byte, error) {
+	type msg struct {
+		Version    uint64
+		To         address.Address
+		From       address.Address
+		Nonce      uint64
+		Value      abi.TokenAmount
+		GasLimit   int64
+		GasFeeCap  abi.TokenAmount
+		GasPremium abi.TokenAmount
+		Method     abi.MethodNum
+		Params     []byte
+	}
+	type fMsg struct {
+		ID string
+
+		UnsignedCid *cid.Cid
+		SignedCid   *cid.Cid
+		msg
+		Signature *crypto.Signature
+
+		Height     int64
+		Confidence int64
+		Receipt    *venusTypes.MessageReceipt
+		TipSetKey  venusTypes.TipSetKey
+		Meta       *MsgMeta
+		WalletName string
+		FromUser   string
+
+		State MessageState
+
+		CreatedAt time.Time
+		UpdatedAt time.Time
+	}
+	return json.Marshal(fMsg{
+		ID:          m.ID,
+		UnsignedCid: m.UnsignedCid,
+		SignedCid:   m.SignedCid,
+		msg: msg{
+			Version:    m.Message.Version,
+			To:         m.Message.To,
+			From:       m.Message.From,
+			Nonce:      m.Message.Nonce,
+			Value:      m.Message.Value,
+			GasLimit:   m.Message.GasLimit,
+			GasFeeCap:  m.Message.GasFeeCap,
+			GasPremium: m.Message.GasPremium,
+			Method:     m.Message.Method,
+			Params:     m.Message.Params,
+		},
+		Signature:  m.Signature,
+		Height:     m.Height,
+		Confidence: m.Confidence,
+		Receipt:    m.Receipt,
+		TipSetKey:  m.TipSetKey,
+		Meta:       m.Meta,
+		WalletName: m.WalletName,
+		FromUser:   m.FromUser,
+		State:      m.State,
+		CreatedAt:  m.CreatedAt,
+		UpdatedAt:  m.UpdatedAt,
+	})
 }
 
 func FromUnsignedMessage(unsignedMsg venusTypes.Message) *Message {
