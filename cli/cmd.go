7fe3519becffee0c4ae0f64668a138d313906097
simlecode <hj1557986@gmail.com>
2022-06-09 13:15:36 +0800

    cmd: Fix incorrect configuration loading

--- cli/cmd.go
+++ cli/cmd.go
@@ -1,10 +1,14 @@
 package cli
 
 import (
+	"path/filepath"
+
 	"github.com/filecoin-project/go-jsonrpc"
+	"github.com/filecoin-project/venus-messager/filestore"
 	"github.com/filecoin-project/venus-messager/service"
 	v1 "github.com/filecoin-project/venus/venus-shared/api/chain/v1"
 	"github.com/ipfs-force-community/venus-common-utils/apiinfo"
+	"github.com/mitchellh/go-homedir"
 	"github.com/urfave/cli/v2"
 
 	"github.com/filecoin-project/venus-messager/config"
@@ -13,7 +17,7 @@ import (
 )
 
 func getAPI(ctx *cli.Context) (messager.IMessager, jsonrpc.ClientCloser, error) {
-	cfg, err := config.ReadConfig(ctx.String("config"))
+	cfg, err := getConfig(ctx)
 	if err != nil {
 		return nil, func() {}, err
 	}
@@ -30,9 +34,18 @@ func getAPI(ctx *cli.Context) (messager.IMessager, jsonrpc.ClientCloser, error) 
 }
 
 func getNodeAPI(ctx *cli.Context) (v1.FullNode, jsonrpc.ClientCloser, error) {
-	cfg, err := config.ReadConfig(ctx.String("config"))
+	cfg, err := getConfig(ctx)
 	if err != nil {
 		return nil, func() {}, err
 	}
 	return service.NewNodeClient(ctx.Context, &cfg.Node)
+}
+
+func getConfig(ctx *cli.Context) (*config.Config, error) {
+	repoPath, err := homedir.Expand(ctx.String("repo"))
+	if err != nil {
+		return nil, err
+	}
+
+	return config.ReadConfig(filepath.Join(repoPath, filestore.ConfigFile))
 }
