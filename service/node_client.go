981a4b95f09a46216148fd3fa71e0d04e6e4520d
simlecode <69969590+simlecode@users.noreply.github.com>
2022-07-18 14:10:23 +0800

    fix: use v1api for node

--- service/node_client.go
+++ service/node_client.go
@@ -7,16 +7,8 @@ import (
 	"github.com/filecoin-project/venus-messager/config"
 
 	v1 "github.com/filecoin-project/venus/venus-shared/api/chain/v1"
-	"github.com/ipfs-force-community/venus-common-utils/apiinfo"
 )
 
 func NewNodeClient(ctx context.Context, cfg *config.NodeConfig) (v1.FullNode, jsonrpc.ClientCloser, error) {
-
-	apiInfo := apiinfo.NewAPIInfo(cfg.Url, cfg.Token)
-	addr, err := apiInfo.DialArgs("v0")
-	if err != nil {
-		return nil, nil, err
-	}
-	res, closer, err := v1.NewFullNodeRPC(ctx, addr, apiInfo.AuthHeader())
-	return res, closer, err
+	return v1.DialFullNodeRPC(ctx, cfg.Url, cfg.Token, nil)
 }
