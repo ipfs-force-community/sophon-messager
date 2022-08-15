01c637c44f22ce015e0f8e3e8cffd08f4fee114f
tanlang <lin.zexiao@qq.com>
2022-08-15 13:10:24 +0800

    feat: add p2p net cli

---
+++ cli/swarm.go
@@ -0,0 +1,96 @@
+package cli
+
+import (
+	"fmt"
+
+	"github.com/filecoin-project/venus/pkg/net"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/urfave/cli/v2"
+)
+
+var SwarmCmds = &cli.Command{
+	Name:  "swarm",
+	Usage: "swarm commands",
+	Subcommands: []*cli.Command{
+		connectCmd,
+		peersCmd,
+	},
+}
+
+var connectCmd = &cli.Command{
+	Name:      "connect",
+	Usage:     "connect to a libp2p node",
+	ArgsUsage: "[peerIds]",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name:    "address",
+			Aliases: []string{"a"},
+			Usage:   "connect with libp2p address rather than peerId",
+		},
+	},
+	Action: func(ctx *cli.Context) error {
+		if ctx.Args().Len() < 1 {
+			return fmt.Errorf("must specify peerId or address")
+		}
+
+		client, closer, err := getAPI(ctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		peers := ctx.Args().Slice()
+
+		if ctx.IsSet("address") {
+			pis, err := net.ParseAddresses(ctx.Context, peers)
+			if err != nil {
+				return err
+			}
+			for _, pi := range pis {
+				if err := client.NetConnect(ctx.Context, pi); err != nil {
+					return err
+				}
+			}
+		} else {
+			for _, p := range peers {
+				peerid, err := peer.Decode(p)
+				if err != nil {
+					return fmt.Errorf("invalid peer id: %s", p)
+				}
+				if pi, err := client.NetFindPeer(ctx.Context, peerid); err != nil {
+					return err
+				} else {
+					if err := client.NetConnect(ctx.Context, pi); err != nil {
+						return err
+					}
+				}
+			}
+		}
+
+		return nil
+	},
+}
+
+var peersCmd = &cli.Command{
+	Name:  "peers",
+	Usage: "list swarm peers",
+	Action: func(ctx *cli.Context) error {
+
+		api, closer, err := getAPI(ctx)
+		if err != nil {
+			return err
+		}
+		defer closer()
+
+		peers, err := api.NetPeers(ctx.Context)
+		if err != nil {
+			return err
+		}
+
+		for _, p := range peers {
+			fmt.Printf("%s, %s\n", p.ID, p.Addrs)
+		}
+
+		return nil
+	},
+}
